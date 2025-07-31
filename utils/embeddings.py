f2e801b0a9b2791359228360367be58bc7fff9fe
husnainli <hossnein7@gmail.com>
2025-07-31 19:46:00 +0500

    new commit

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -1,5 +1,5 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 import os
 
@@ -11,17 +11,23 @@ def chunk_text(text, chunk_size=500, overlap=100):
     )
     return splitter.split_text(text)
 
-def embed_chunks(chunks, persist_dir="chroma_db", model_name='intfloat/multilingual-e5-base'):
+# def embed_chunks(chunks, persist_dir="chroma_db", model_name='intfloat/multilingual-e5-base'):
+#     embedding = HuggingFaceEmbeddings(model_name=model_name)
+
+#     # Ensure reproducible index
+#     if os.path.exists(persist_dir):
+#         vectorstore = Chroma(embedding_function=embedding)
+#     else:
+#         vectorstore = Chroma.from_texts(chunks, embedding=embedding, persist_directory=persist_dir)
+#         vectorstore.persist()
+
+#     return vectorstore
+
+def embed_chunks(chunks, model_name='intfloat/multilingual-e5-base'):
     embedding = HuggingFaceEmbeddings(model_name=model_name)
-
-    # Ensure reproducible index
-    if os.path.exists(persist_dir):
-        vectorstore = Chroma(embedding_function=embedding)
-    else:
-        vectorstore = Chroma.from_texts(chunks, embedding=embedding, persist_directory=persist_dir)
-        vectorstore.persist()
-
+    vectorstore = FAISS.from_texts(chunks, embedding)
     return vectorstore
+
 
 
 def retrieve_similar_chunks(vectorstore, query, k=4):
